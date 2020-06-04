d2e470e6202cfa7788decdf54baca777e6da7d9f
Alberto Cavalcanti <cavalcanti_alberto@yahoo.com.br>
2020-06-04 18:11:51 -0300

    Criando as classes

---
+++ HerancaPolimorfismo/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace HerancaPolimorfismo.Entities
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public Product() { }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public virtual string priceTag()
+        {
+            return Name + " $ " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
